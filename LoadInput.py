90e25d0b3dd8d79a1347b737ba29a633eece4e78
lethanhh <lethanhh@fit.cvut.cz>
2019-04-10 23:16:47 +0200

    Loading data from csv

--- LoadInput.py
+++ LoadInput.py
@@ -1,1 +1,13 @@
-print("test")
+import csv
+
+with open('data/movies.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\tID: {row[0]}, {row[1]} is in following genres: {row[2]}')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
