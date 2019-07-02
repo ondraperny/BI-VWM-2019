c082765e52491f772239be1412057512db77ff06
ondra <pernyond@fit.cvut.cz>
2019-07-03 01:41:10 +0200

    API described

--- Main.py
+++ Main.py
@@ -2,28 +2,25 @@ from flaskr import Algorithm
 
 
 def main():
-    """Poznamky k API, vsechno co vracim uz je serazeny jak ma byt, takze staci jen vypsat,
-    argumenty funkci jsou vzdy u kazde funkce napsane,
-    co funkce vraceji vzdy popsano v komentari nad danou funkci,
-    jiny nez tyhle 4 fce + konstruktor bys nemel potrebovat"""
+    """Description of backend API, all results are sorted, ready to be only printed"""
 
-    # prihlaseni uzivatele bude odpovidat vytvoreni classy Recommendation pro toho daneho uzivatele
-    # tento konstruktor ma jeste dalsich 7 parametru(vsechny maji defaultni hodnoty, a jsou popsane v konstruktoru
-    # recommendation classy, tyto parametry se daji menit v administratorskem modulu
-    recommend = Algorithm.Recommendation(1)
+    # user login equal creation of Recommendation class, where constructor's mandatory parameter is user's ID,
+    # there are 7 other arbitrary parameters(parameters that alternate how recommending works)
+    # all has default values and are described in detail in constructor
+    # recommend = Algorithm.Recommendation(1)
 
-    # vrati list listů s 3 hodnotami [movie_id, movie_name, movie_rating] pro vsechny filmi, ktere uzivatel ohodnotil
+    # return list of lists with 3 values: [movie_id, movie_name, movie_rating] for every movie that user rated
     # recommend.main_user_ratings()
 
-    # vrati list listů s 3 hodnotami [movie_id, movie_name, expected_rating], kde expected_rating je cislo vyjadrujici
-    # predpoklad ja by dany uzivatel ohodnotil tento film (urcuje prioritu pro recommendaci)
-    recommend.final_recommendation()
+    # return list of lists with 3 values: [movie_id, movie_name, expected_rating], where expected_rating is number
+    # expressing assumption how would given user value this movie(which determines priority for recommendation)
+    # recommend.final_recommendation()
 
-    # vraci dictionary s movie_id a movie_name
+    # return dictionary with movie_id and movie_name
     # recommend.all_movies()
 
-    # vraci True pokud vstupni argumenty byli vporadku (rating v mezich, existuje zadane movie_id),
-    # pokud vporadku nebyli vraci False
+    # change database record - return True if input arguments are valid (rating in boundaries, given movie_id exists)
+    # otherwise return False
     # print(recommend.change_database(movie_id, new_rating))
 
 
